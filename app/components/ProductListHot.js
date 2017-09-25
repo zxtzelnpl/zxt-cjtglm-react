6d8cb22da64151e3418b408d93551c644e2d04cf
was <zxt_zel_npl@163.com>
2017-09-25 14:21:31 +0800

    divice the scrollText

--- app/components/ProductListHot.js
+++ app/components/ProductListHot.js
@@ -6,34 +6,6 @@ import {formatDate} from '../static/js/tools'
 
 import './ProductListHot.less'
 
-class ScrollText extends React.Component{
-    constructor(props, context) {
-        super(props, context);
-        this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this)
-    }
-
-    render() {
-        let datas = this.props.list
-        let linkData = datas[0]
-        let textsHtml = datas.map((data)=>{
-            let {stockName,time,word,rise} = data.stock
-            return (
-                <span key={data.name}>{time.slice(5)}《选股牛人-<b>{data.name}</b>》推出<b>{stockName}</b>{word}<b>{rise}</b></span>
-            )
-        })
-        let textLinkHtml = (<span key="last">{linkData.stock.time.slice(5)}《选股牛人-<b>{linkData.name}</b>》推出<b>{linkData.stock.name}</b>{linkData.stock.word}<b>{linkData.stock.rise}</b></span>)
-
-        return (
-            <div className="scroll-text">
-                <div className="wrap">
-                    {textsHtml}
-                    {textLinkHtml}
-                </div>
-            </div>
-        )
-    }
-}
-
 
 class Product extends React.Component {
     constructor(props, context) {
@@ -94,7 +66,6 @@ class ProductListHot extends React.Component {
                 <div className="product-hot">
                     <div className="title">
                         <span className="word">明星牛人</span>
-                        <ScrollText list = {this.props.list}/>
                     </div>
                     <div className="list">
                         <ul>

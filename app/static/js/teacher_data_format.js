ff61f2b8d034ff4aa6503aeb7302112a453a54ee
was <zxt_zel_npl@163.com>
2017-11-23 17:27:02 +0800

    add wuweiwei3

--- app/static/js/teacher_data_format.js
+++ app/static/js/teacher_data_format.js
@@ -1,5 +1,5 @@
 import chart_format from './chart_format'
-import {stocks,ranks} from './teacher_stock'
+import {stocks,ranks,recordNum} from './teacher_stock'
 import teacher_position from './teacher_position'
 
 export default function(products){
@@ -20,6 +20,7 @@ export default function(products){
         product.records = records;
         product.stocks = stocks(product.name)
         product.rank = ranks(product.name)
+        product.recordNum = recordNum(product.name)
         product.lables = teacher_position(product.position)//取标签后面的3个
     })
 }
--- app/static/js/teacher_stock.js
+++ app/static/js/teacher_stock.js
@@ -22,12 +22,12 @@ let maxin = [
     {
         name: '四维图新',
         code: '002405',
-        result: '21.68%',
+        result: '27.28%',
         daySend: '2017.11.02',
-        day: '7',
-        dayCount: '七日最高涨幅',
+        day:'阶段',
+        dayCount: '阶段最高涨幅',
         img: maxin2
-    },
+    }/*,
     {
         name: '南卫股份',
         code: '603880',
@@ -36,7 +36,7 @@ let maxin = [
         day: '2',
         dayCount: '二日最高涨幅',
         img: maxin1
-    }
+    }*/
 ]
 let suxuepeng = [{
     name: '陕西黑猫',
@@ -58,7 +58,7 @@ let wuweiwei = [
         day:'2',
         dayCount: '两日最高涨幅',
         img:wuweiwei3
-    },
+    }/*,
     {
         name: '嘉诚国际',
         code: '603535',
@@ -76,7 +76,7 @@ let wuweiwei = [
         day: '阶段',
         dayCount: '阶段最高涨幅',
         img: wuweiwei1
-    }
+    }*/
 ]
 
 let yuliang = [{
@@ -179,5 +179,18 @@ export function ranks(name) {
             return 3
         default:
             return 4
+    }
+}
+
+export function recordNum(name){
+    switch (name) {
+        case '吴伟伟':
+            return 1
+        case '马鑫':
+            return 0
+        case '周煜':
+            return 0
+        default:
+            return 0
     }
 }
